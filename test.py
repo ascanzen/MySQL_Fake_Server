3571854da2a91baa8be5216035c9288ac835e3c0
ascanzen <ascanzen@gmail.com>
2023-06-01 17:28:01 +0800

    add column descript

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import FieldType
 import time
 
 # Connect to server
@@ -12,8 +13,16 @@ cur = cnx.cursor()
 # Execute a query
 cur.execute("select a,b,c from book")
 
+for i in range(len(cur.description)):
+    print("Column {}:".format(i + 1))
+    desc = cur.description[i]
+    print("  column_name = {}".format(desc[0]))
+    print("  type = {} ({})".format(desc[1], FieldType.get_info(desc[1])))
+    print("  null_ok = {}".format(desc[6]))
+    print("  column_flags = {}".format(desc[7]))
+
 # # Fetch one result
-row = cur.fetchone()
+row = cur.fetchall()
 print("Current date is: {0}".format(row))
 
 # time.sleep(1000)
